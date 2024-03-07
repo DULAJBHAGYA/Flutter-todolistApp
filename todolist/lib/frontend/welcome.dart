759b7aeba36f30ca95e366e7c25e4e393b0b4515
Dulaj Bhagya Upananda <95066839+DULAJBHAGYA@users.noreply.github.com>
2024-03-07 17:40:34 +0530

    7th march first commit

---
+++ todolist/lib/frontend/welcome.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/frontend/color.dart';
+import 'package:todolist/frontend/home.dart';
+
+void main() {
+  runApp(Welcome());
+}
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            welcomeText(),
+            welcomeButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  OutlinedButton welcomeButton(BuildContext context) {
+    return OutlinedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Home()),
+              );
+            },
+            child: Text(
+              'Click here to continue',
+              style: TextStyle(
+                fontSize: 18,
+                color: tdBlue, 
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            style: ButtonStyle(
+              side: MaterialStateProperty.all(BorderSide(width: 2.0)),
+            ),
+          );
+
+  }
+
+  Widget welcomeText() {
+    return Text(
+      "Hello !",
+      style: TextStyle(
+        fontSize: 40.0,
+        fontWeight: FontWeight.bold,
+        color: tdBlue,
+      ),
+    );
+  }
+}
